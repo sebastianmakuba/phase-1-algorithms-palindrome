82e10ca6a3e64b9e579fb9dcb843a482133719ef
+ <+>
2023-06-29 10:30:29 +0300

    completed  assignement

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedStr = word.split('').reverse().join('')
+  return word === reversedStr  
 }
 
 /* 
   Add your pseudocode here
+  split the word into characters
+  reverse the word
+  join the split word
 */
 
 /*
   Add written explanation of your solution here
+  the function splits the word into individual characters.
+  it then reverses to check if its a palindrome.
+  it joins the split word to make a word 
+  then cecks if its the same as the originall
 */
 
 // You can run `node index.js` to view these console logs
